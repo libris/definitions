1a21a1f2ef8bb11c4d2712aca0552e95bdafe232
Niklas Lindström <lindstream@gmail.com>
2015-02-20 17:38:48 +0100

    Add initial SAB-to-SKOS script

---
+++ scripts/create_sab_skos_data.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+import csv
+import re
+import sys
+from rdflib import *
+from rdflib.namespace import SKOS
+import urllib
+
+
+SAB = Namespace("http://id.kb.se/def/scheme/sab/")
+DDC = Namespace("http://dewey.info/class/")
+LANG = 'sv'
+
+hint_link_map = {
+    #'#H': SKOS.closeMatch,
+    #'#G': SKOS.closeMatch,
+    #'#M': ~SKOS.closeMatch,
+    '#1': SKOS.exactMatch,
+    '#2': SKOS.broadMatch,
+    '#3': SKOS.narrowMatch,
+    '#4': SKOS.closeMatch,
+    'DDK22': SKOS.relatedMatch, # TODO
+}
+
+def to_uri(scheme, code):
+    return scheme[urllib.quote(code.encode('utf-8'), safe=b':(),')]
+
+
+def create_sab_skos_data(graph, fpath, limit=0):
+    for i, (code, label) in enumerate(read_csv_items(fpath)):
+        r = graph.resource(to_uri(SAB, code))
+        r.add(RDF.type, SKOS.Concept)
+        r.add(SKOS.notation, Literal(code))
+        r.add(SKOS.prefLabel, Literal(label, lang=LANG))
+        if limit and i > limit:
+            break
+
+
+def create_sab_ddc_data(graph, fpath, limit=0):
+    for number, sab_code, ddc_code, hint in read_csv_items(
+            fpath, skip_comment=True, coding='utf-8', size=4):
+        hint = re.split(r'\s+|\w(?=#)', hint)[-1].strip()
+        link = hint_link_map.get(hint)
+        if not link:
+            print >> sys.stderr, "No link map for", hint.encode('utf-8')
+            continue
+        r = graph.resource(to_uri(SAB, sab_code))
+        r.add(link, to_uri(DDC, ddc_code))
+
+
+def read_csv_items(fpath, skip_first=True, skip_comment=False,
+        csv_dialect='excel-tab', coding='latin-1', size=0):
+    with open(fpath, 'rb') as fp:
+        reader = csv.reader(fp, csv_dialect)
+        if skip_first is True:
+            reader.next()
+        for row in reader:
+            if not row or skip_comment and row[0][0] == '#':
+                continue
+            cols = [col.strip().decode(coding) for col in row]
+            if size and len(cols) > size:
+                cols = cols[0:size]
+            yield cols
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    sab_codes_fpath = args.pop(0)
+    ddc_map_fpath = args.pop(0)
+    limit = int(args.pop(0)) if args else 0
+
+    graph = Graph()
+    graph.namespace_manager.bind('skos', SKOS)
+    create_sab_skos_data(graph, sab_codes_fpath, limit=limit)
+    create_sab_ddc_data(graph, ddc_map_fpath, limit=limit)
+    graph.serialize(sys.stdout, format='turtle')
