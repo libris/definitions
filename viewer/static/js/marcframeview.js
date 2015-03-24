2480c372845e77eb8cfddba50f12c1019a33b4cd
Niklas Lindström <lindstream@gmail.com>
2015-03-24 18:57:57 +0100

    Use AJAX-based embedded vocab viewer

--- viewer/static/js/marcframeview.js
+++ viewer/static/js/marcframeview.js
@@ -1,25 +1,37 @@
 $(function () {
 
   var loaded = false;
-  vocabview = $('iframe[name=vocabview]');
-  vocabview.hide();
+  var embedvocab = $('<div id="embedvocab"></div>').appendTo('body');
 
-  vocabview.load(function () {
-    var contents = $(this).contents();
-    $('body', contents).addClass('compact');
+  $(document).on('click', 'body', function (event) {
+    $('.active').removeClass('active');
   });
 
-  $('body').on('click', function () {
-    vocabview.hide();
-  });
-
-  $('a[href^="../vocabview"]').click(function (event) {
-    if (!loaded) {
-      vocabview.attr('src', "/vocabview/");
-      loaded = true;
-    }
-    vocabview.show();
+  $(document).on('click', 'article.panel', function (event) {
     event.stopPropagation();
+  });
+
+  var termLinkSel = 'a[href^="../vocabview"], article.panel a[href^="#"]';
+  $(document).on('click', termLinkSel, function () {
+    var ref = $(this).attr('href').replace(/[^#]*#(.*)/, "$1");
+    function display() {
+      $('.active').removeClass('active');
+      $('#' + ref).addClass('active');
+    }
+    if (!loaded) {
+      embedvocab.append('<article class="panel active text-center">' +
+                        '<i class="glyphicon glyphicon-refresh btn-lg"></i></article>');
+      embedvocab.load("/vocabview/ article.panel[id]", function() {
+        $('a[href^="http"]').attr('target', '_blank').click(function (event) {
+          event.stopPropagation();
+        });
+        display();
+      });
+      loaded = true;
+    } else {
+      display();
+    }
+    return false;
   });
 
 });
