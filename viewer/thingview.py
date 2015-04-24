c907cb6bdee7c9b951bd35d8d1570832b3e90a7e
Niklas Lindström <lindstream@gmail.com>
2015-04-24 17:48:40 +0200

    Improve thingview

--- viewer/thingview.py
+++ viewer/thingview.py
@@ -1,27 +1,60 @@
-from rdflib import RDFS
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from rdflib import RDF, RDFS
 from flask import Blueprint, render_template
 
 app = Blueprint('thingview', __name__)
 
-ID, TYPE = '@id', '@type'
+ID, TYPE, REV = '@id', '@type', '@reverse'
 
 vocab = {
+    '@type': {
+        ID: RDF.type,
+        'curie': 'rdf:type',
+        'label': "Typ"
+    },
+    'Thing': {
+        ID: 'Thing',
+        'curie': 'Thing owl:Thing',
+        'label': 'Sak'
+    },
     'label': {
         ID: RDFS.label,
         'curie': 'rdfs:label name',
         'label': "Etikett"
+    },
+    'seeAlso': {
+        ID: RDFS.seeAlso,
+        'curie': 'rdfs:seeAlso',
+        'label': "Se även"
     }
 }
 
-ld_context = {'ID': ID,'TYPE': TYPE, 'vocab': vocab}
+db = {
+    'something': {
+        ID: 'something',
+        TYPE: 'Thing',
+        'label': 'Something'
+    },
+    'other': {
+        ID: 'other',
+        TYPE: 'Thing',
+        'label': 'Other',
+    }
+}
+db['something']['seeAlso'] = db['other']
+db['other'][REV] = {'seeAlso': [{k: v for k, v in db['something'].items() if k != 'seeAlso'}]}
 
+ui_defs = {
+    REV: {
+        'label': "Saker som länkar hit"
+    }
+}
+
+ld_context = {'ID': ID,'TYPE': TYPE, 'REV': REV, 'vocab': vocab, 'ui': ui_defs}
 app.context_processor(lambda: ld_context)
 
-@app.route('/view/<path:path>')
+@app.route('/page/<path:path>')
 def thingview(path):
-    thing = {
-        ID: path,
-        TYPE: 'Thing',
-        'label': 'Nothing'
-    }
+    thing = db[path]
     return render_template('thing.html', thing=thing)
