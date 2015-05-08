2cd39156235bdf9d53af6f2f8e6fdacdeef9ebe2
Niklas Lindström <lindstream@gmail.com>
2015-05-08 18:07:37 +0200

    Refactor datatools to define Vocab and DB classes

--- util/datatools.py
+++ util/datatools.py
@@ -1,14 +1,72 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
+__metaclass__ = type
+
 import logging
 import os
 import json
-from rdflib import Graph, URIRef, Namespace, RDF, RDFS
+from rdflib import Graph, URIRef, Namespace, RDF, RDFS, OWL
 
 
 logger = logging.getLogger(__name__)
 
 ID, TYPE, REV = '@id', '@type', '@reverse'
+
+
+class Vocab:
+
+    def __init__(self, vocab_source, vocab_uri, lang):
+        self.index = {}
+        label_key_items = []
+
+        BASE_LABEL = URIRef(vocab_uri + 'label')
+        g = Graph().parse(vocab_source, format='turtle')
+
+        for s in set(g.subjects()):
+            if not isinstance(s, URIRef):
+                continue
+            if not s.startswith(vocab_uri):
+                continue
+
+            key = s.replace(vocab_uri, '')
+
+            label = key
+            for label in g.objects(s, RDFS.label):
+                if label.language == lang:
+                    break
+            if label:
+                label = unicode(label)
+
+            term = {ID: unicode(s),'label': label, 'curie': key}
+
+            if (s, RDF.type, OWL.ObjectProperty) in g:
+                term[TYPE] = ID
+
+            self.index[key] = term
+
+            label_distance = path_distance(g, s, RDFS.subPropertyOf, BASE_LABEL)
+            if label_distance is not None:
+                label_key_items.append((label_distance, key))
+
+        self.label_keys = [key for ldist, key in sorted(label_key_items, reverse=True)]
+
+    def sortedkeys(self, thing):
+        def keykey(key):
+            if key.startswith('@'):
+                return (0, key)
+            try:
+                return (self.label_keys.index(key), key)
+            except ValueError:
+                weight = 200 if self.index[key].get(TYPE) == ID else 100
+                return (weight, key)
+        return sorted(thing, key=keykey)
+
+    def labelgetter(self, item):
+        for lkey in self.label_keys:
+            label = item.get(lkey)
+            if label:
+                return label
+        return item.get('@id')
 
 
 def path_distance(g, s, p, base):
@@ -47,73 +105,46 @@ def path_distance(g, s, p, base):
     return find_path(s)
 
 
-def load_vocab(vocab_source, vocab_uri, lang):
-    vocab = {}
-    label_keys = []
+class DB:
 
-    BASE_LABEL = URIRef(vocab_uri + 'label')
-    g = Graph().parse(vocab_source, format='turtle')
-    for s in set(g.subjects()):
-        if not isinstance(s, URIRef):
-            continue
-        if not s.startswith(vocab_uri):
-            continue
-        key = s.replace(vocab_uri, '')
-        label = key
-        for label in g.objects(s, RDFS.label):
-            if label.language == lang:
-                break
-        if label:
-            label = unicode(label)
-        term = {ID: unicode(s),'label': label, 'curie': key}
-        vocab[key] = term
-        label_distance = path_distance(g, s, RDFS.subPropertyOf, BASE_LABEL)
-        if label_distance is not None:
-            label_keys.append((label_distance, key))
-    label_keys = [key for ldist, key in sorted(label_keys, reverse=True)]
+    def __init__(self, db_source, vocab):
+        self.index = {}
+        self.same_as = {}
 
-    return vocab, label_keys
+        if not os.path.isfile(db_source):
+            logger.warn("DB source %s does not exist", db_source)
+            return
 
+        for l in open(db_source):
+            data = json.loads(l)
+            data.pop('_marcUncompleted', None)
+            item = data.pop('about')
+            item['describedBy'] = data
+            self.index[item[ID]] = item
+            if 'sameAs' in item:
+                for ref in item.get('sameAs'):
+                    if ID in ref:
+                        self.same_as[ref[ID]] = item[ID]
 
-def load_db(db_source, label_keys):
-    db = {}
-    same_as = {}
-
-    if not os.path.exists(db_source):
-        logger.warn("DB source %s does not exist", db_source)
-        return
-
-    for l in open(db_source):
-        data = json.loads(l)
-        data.pop('_marcUncompleted', None)
-        item = data.pop('about')
-        item['describedBy'] = data
-        db[item[ID]] = item
-        for l_key in label_keys:
-            if l_key in item:
-                item['label'] = item[l_key]
-                break
-        if 'sameAs' in item:
-            for ref in item.get('sameAs'):
-                if ID in ref:
-                    same_as[ref[ID]] = item[ID]
-
-    chip_keys = [ID, TYPE] + label_keys
-    for other in db.values():
-        chip = {k: v for k, v in other.items() if k in chip_keys}
-        for link, refs in other.items():
-            if link == 'sameAs':
-                continue
-            if not isinstance(refs, list):
-                refs = [refs]
-            for ref in refs:
-                if isinstance(ref, dict) and ID in ref:
-                    item = db.get(ref[ID])
-                    if not item:
+        chip_keys = [ID, TYPE] + vocab.label_keys
+        for other in self.index.values():
+            chip = {k: v for k, v in other.items() if k in chip_keys}
+            for link, refs in other.items():
+                if link == 'sameAs':
+                    continue
+                if not isinstance(refs, list):
+                    refs = [refs]
+                for ref in refs:
+                    if not isinstance(ref, dict):
                         continue
-                    item.setdefault(REV, {}).setdefault(link, []).append(chip)
-
-    return db, same_as
+                    ref_id = ref.get(ID)
+                    if ref_id:
+                        item = self.index.get(ref_id) or self.index.get(
+                                self.same_as.get(ref_id))
+                        if not item:
+                            continue
+                        item.setdefault(REV, {}
+                                ).setdefault(link, []).append(chip)
 
 
 if __name__ == '__main__':
