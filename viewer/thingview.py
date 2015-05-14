7c6f492684e5a7db509df281d6d0f564faebf76c
Niklas Lindström <lindstream@gmail.com>
2015-05-14 14:45:36 +0200

    Add content negotiation

--- viewer/thingview.py
+++ viewer/thingview.py
@@ -2,10 +2,15 @@
 from __future__ import unicode_literals
 import glob
 import itertools
+import json
 from operator import itemgetter
-from flask import Blueprint, render_template, redirect, abort
-from util.ld import RDF, Vocab, ID, TYPE, REV
+from flask import Blueprint, request, Response, render_template, redirect, abort
+from util.ld import Graph, RDF, Vocab, ID, TYPE, REV
 from util.db import DB
+from .conneg import Negotiator
+
+
+BASE_URI = "http://id.kb.se/"
 
 ui_defs = {
     REV: {
@@ -39,7 +44,8 @@ def setup_app(setup_state):
     }
     app.context_processor(lambda: ld_context)
 
-@app.route('/list/', defaults=dict(chunk=10000))
+
+@app.route('/list/', defaults={'chunk': 10000})
 @app.route('/list/<int:chunk>')
 def listview(chunk):
     typegetter = itemgetter(TYPE)
@@ -47,8 +53,16 @@ def listview(chunk):
     type_groups = itertools.groupby(sorted(items, key=typegetter), typegetter)
     return render_template('list.html', item_groups_by_type=type_groups)
 
-@app.route('/<path:path>/data.html')
-def thingview(path):
+@app.route('/def/terms/<term>')
+def termview(term):
+    return redirect('/vocabview#' + term, 303)
+
+
+negotiator = Negotiator()
+
+@app.route('/<path:path>/data')
+@app.route('/<path:path>/data.<suffix>')
+def thingview(path, suffix=None):
     if not path.startswith(('/', 'http:', 'https:')):
         path = '/' + path
     if path in db.same_as:
@@ -56,8 +70,36 @@ def thingview(path):
     thing = db.get_item(path)
     if not thing:
         return abort(404)
-    return render_template('thing.html', thing=thing)
 
-@app.route('/def/terms/<term>')
-def termview(term):
-    return redirect('/vocabview#' + term, 303)
+    mimetype, render = negotiator.negotiate(request, suffix)
+    if not render:
+        return abort(406)
+
+    result = render(path, thing)
+    return Response(result, mimetype=mimetype) if isinstance(
+            result, bytes) else result
+
+@negotiator.add('text/html', 'html')
+@negotiator.add('application/xhtml+xml', 'xhtml')
+def render_html(path, data):
+    return render_template('thing.html', path=path, thing=data)
+
+@negotiator.add('application/ld+json', 'jsonld')
+@negotiator.add('application/json', 'json')
+@negotiator.add('text/json')
+def render_jsonld(path, data):
+    return json.dumps(data, indent=2, sort_keys=True,
+            separators=(',', ': '), ensure_ascii=False).encode('utf-8')
+
+@negotiator.add('text/turtle', 'ttl')
+def render_ttl(path, data):
+    return to_graph(data).serialize(format='turtle')
+
+@negotiator.add('application/rdf+xml', 'rdf')
+@negotiator.add('text/xml', 'xml')
+def render_xml(path, data):
+    return to_graph(data).serialize(format='pretty-xml')
+
+def to_graph(data):
+    return Graph().parse(data=json.dumps(data), base=BASE_URI,
+            format='json-ld', context="build/lib-context.jsonld")
