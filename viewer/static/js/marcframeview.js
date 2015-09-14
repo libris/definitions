7debc0400041d9f6c9b38234d885e2ca99281642
Ola Blissing <ola.blissing@gmail.com>
2015-09-14 14:30:38 +0200

    Vocabviewer positioning Fixes #2

--- viewer/static/js/marcframeview.js
+++ viewer/static/js/marcframeview.js
@@ -10,16 +10,25 @@ $(function () {
   $(document).on('click', 'article.panel', function (event) {
     event.stopPropagation();
   });
-
-  var termLinkSel = 'a[href^="../vocabview"], article.panel a[href^="#"]';
-  $(document).on('click', termLinkSel, function () {
-    var ref = $(this).attr('href').replace(/[^#]*#(.*)/, "$1");
+  
+    var termLinkSel = 'a[href^="../vocabview"]';
+    var articleLinkSel = 'article.panel a[href^="#"]';
+    var linkPos = {};
+    
+  function openTerm(ref, insidePopover) {
+    
     function display() {
       $('.state-active').removeClass('state-active');
       $('#' + ref).addClass('state-active');
+      $('.state-active').css('top', linkPos.Y - ($('.state-active').height()/2) + 8 + "px");
+      $('.state-active').css('left', (linkPos.X + 10) + "px");
+      if(insidePopover)
+        $('.state-active .arrow').css('display', 'none');
+      else
+        $('.state-active .arrow').css('display', 'block');
     }
     if (!loaded) {
-      embedvocab.append('<article class="panel state-active text-center">' +
+      embedvocab.append('<article class="panel text-center">' +
                         '<i class="glyphicon glyphicon-refresh btn-lg"></i></article>');
       embedvocab.load("/vocabview/ article.panel[id]", function() {
         $('article.panel[id]', this).
@@ -35,6 +44,24 @@ $(function () {
     } else {
       display();
     }
+    
+  }
+    
+  $(document).on('click', articleLinkSel, function (e) {
+    e.preventDefault();
+    var ref = $(this).attr('href').replace(/[^#]*#(.*)/, "$1");
+    
+    openTerm(ref, true);
+  });
+    
+  $(document).on('click', termLinkSel, function (e) {
+    var ref = $(this).attr('href').replace(/[^#]*#(.*)/, "$1");
+    
+    linkPos.X = $(this).offset().left + $(this).outerWidth();
+    linkPos.Y = $(this).offset().top;
+    
+    openTerm(ref, false);
+    
     return false;
   });
 
