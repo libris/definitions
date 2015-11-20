b221b715e16abfdf271e24a1fe98e2bbaa006db0
Niklas Lindström <lindstream@gmail.com>
2015-11-20 20:14:56 +0100

    Compute common key in analyze-enums.py

--- scripts/analyze-enums.py
+++ scripts/analyze-enums.py
@@ -1,3 +1,5 @@
+import re
+from collections import OrderedDict
 import json
 
 enums = json.load(open('source/enums.jsonld'))
@@ -5,11 +7,9 @@ enums = json.load(open('source/enums.jsonld'))
 collection_sets = set()
 coll_values = {}
 
-for item in enums['enumDefs'].values():
+for item in enums.get('@graph') or enums['enumDefs'].values():
     colls = item.get('inCollection')
-    if colls and len(colls) > 2:
-        #print "\n    ".join(c['@id'] for c in [item] + colls)
-        #print
+    if colls:
         for c in colls:
             coll_values.setdefault(c['@id'], []).append(item['@id'])
         collection_sets.add(tuple(sorted(c['@id'] for c in colls)))
@@ -17,18 +17,39 @@ for item in enums['enumDefs'].values():
 value_sets = {}
 
 for cset in collection_sets:
-    #prev_members = None
     for c in cset:
-        name = c.replace('/collection/marc/', '')
+        name = c
         members = set(coll_values[c])
         value_sets.setdefault(tuple(sorted(members)), set()).add(name)
-        #print name,
-        #if prev_members and members == prev_members:
-        #    print '(same)',
-        #prev_members = members
-    #print
+
+def common_key(terms):
+    prev = set()
+    keys = []
+    for term in terms:
+        key = term.rsplit('/', 1)[-1]
+        keys.append(key)
+        parts = set(re.findall(r'[A-Z][^A-Z]*', term))
+        candidates = prev & parts if prev else parts
+        if not candidates:
+            candidates = parts | prev
+        prev = candidates
+    return "Or".join(keys) if len(candidates) > 1 else next(iter(candidates))
+
+equivs = OrderedDict()
+for values, equiv_colls in value_sets.items():
+    if len(equiv_colls) > 1:
+        equivs[common_key(equiv_colls)] =\
+                [{'values': values}] + \
+                list(equiv_colls)
+
+def dump(data):
+    print(json.dumps(data, indent=2, sort_keys=True, separators=(',', ': ')))
 
 print "# Equivalent collections:"
-for equiv_colls in value_sets.values():
-    if len(equiv_colls) > 1:
-        print ", ".join(equiv_colls)
+dump(equivs)
+
+print "# Map:"
+cmap = {}
+for key, paths in equivs.items():
+    cmap[key] = [path.rsplit('/', 1)[-1] for path in paths if isinstance(path, unicode)]
+dump(cmap)
