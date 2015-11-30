c4d59f8d9383afb8d6c8bb9e0e2792acaeee32b5
Ola Blissing <ola.blissing@gmail.com>
2015-11-30 17:26:13 +0100

    prevent expanded chip from leaving viewport

--- viewer/static/js/thing.js
+++ viewer/static/js/thing.js
@@ -3,18 +3,24 @@ $(function () {
   $('.link-item').each(function() {
     $(this).prepend('<div class="chip-expanded">'+ $(this).html() +'</div>');
     var chip = $(this).find('.chip-expanded');
-    //var x = $(this).offset().left;
-    //if (x + chip.width() > $(document.body).width()) {
-    //  chip.css('right', 0);
-    //}
   });
 
   $('.link-item').hover(function() {
     var chip = $(this).find('.chip-expanded');
+    var parent = $(this);
+    
+    // Calculate if we're outside viewport
+    var diff = (parent.offset().left + chip.width()) - $(window).width()
+    var xMove = 0;
+    if(diff > 0) {
+      xMove += diff + 20;
+    }
+    
     chip.addClass('to-be-active');
     setTimeout(function() {
       if(chip.hasClass('to-be-active'))
         chip.addClass('active').removeClass('to-be-active');
+        chip.css('margin-left', (-xMove));
     }, 500);
   }, function() {
     var chip = $(this).find('.chip-expanded');
