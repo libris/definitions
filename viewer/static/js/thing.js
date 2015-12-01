0af05bd8d5dfd2c5da77131ff927ee4e5e99d5f9
Ola Blissing <ola.blissing@gmail.com>
2015-12-01 10:52:55 +0100

    Adjusted expansion of chip

--- viewer/static/js/thing.js
+++ viewer/static/js/thing.js
@@ -11,7 +11,7 @@ $(function () {
     
     // Calculate if we're outside viewport
     var diff = (parent.offset().left + chip.width()) - $(window).width()
-    var xMove = 0;
+    var xMove = 1;
     if(diff > 0) {
       xMove += diff + 20;
     }
@@ -21,7 +21,7 @@ $(function () {
     setTimeout(function() {
       if(chip.hasClass('to-be-active'))
         chip.addClass('active').removeClass('to-be-active');
-    }, 500);
+    }, 250);
   }, function() {
     var chip = $(this).find('.chip-expanded');
     chip.removeClass('active').removeClass('to-be-active');
