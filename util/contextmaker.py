19b8868323d9be150bb893a9e3014ccc02adbe14
Niklas Lindström <lindstream@gmail.com>
2015-12-17 14:20:24 +0100

    Prefer MADS over SKOS in contextmaker

--- util/contextmaker.py
+++ util/contextmaker.py
@@ -8,7 +8,7 @@ Resource.id = Resource.identifier
 SUFFIX_FORMAT_MAP['jsonld'] = 'json-ld'
 
 
-DEFAULT_NS_PREF_ORDER = 'dc sdo skos prov bf bibo foaf dctype owl rdfs rdf xsd edtf mads'.split()
+DEFAULT_NS_PREF_ORDER = 'dc sdo mads skos prov bf bibo foaf dctype owl rdfs rdf xsd edtf'.split()
 
 CLASS_TYPES = {RDFS.Class, OWL.Class, RDFS.Datatype}
 PROP_TYPES = {RDF.Property, OWL.ObjectProperty, OWL.DatatypeProperty}
