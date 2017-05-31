223bca55e7c03c199a739f16c5e121f5b0119cbd
Niklas Lindström <lindstream@gmail.com>
2017-05-31 17:30:05 +0200

    Tweak marcframe parsing scripts (to be decommissioned)

--- scripts/digest-marcframe.py
+++ scripts/digest-marcframe.py
@@ -1,11 +1,20 @@
+from __future__ import unicode_literals, print_function
+try: str, bytes = unicode, str
+except: pass
 import json
 from itertools import chain
 
 
 def digest_marcframe(marcframe):
+    print('    VALUES (?property ?class ?enumclass) {')
     digest_fixedfields(marcframe)
+    print('    }')
+    print()
+
+    print('    VALUES (?property ?class ?enumclass) {')
     for part in ['patterns', 'bib', 'auth', 'hold']:
-        pass
+        digest_fields(marcframe, part)
+    print('    }')
 
 
 def digest_fixedfields(marcframe):
@@ -25,19 +34,19 @@ def digest_fixedfields(marcframe):
         for col_key, col_dfn in coldefs.items():
             if not isinstance(col_dfn, dict):
                 continue
-            prop = col_dfn.get('link') or col_dfn.get('addLink')
+            link = col_dfn.get('link') or col_dfn.get('addLink')
             tokenmap = col_dfn.get('tokenMap')
-            if not prop or not tokenmap:
+            if not link or not tokenmap:
                 continue
             if col_key.startswith('['):
                 bot, ceil = map(int, col_key[1:-1].split(':'))
                 col_key = str(bot) if ceil == bot + 1 else '{}_{}'.format(bot, ceil - 1)
 
             marcsource = 'marc:{part}/{tag}/{col_key}'.format(**vars())
-            yield (prop, basename, tokenmap, marcsource)
+            yield (link, basename, tokenmap, marcsource)
 
     colprops = {}
-    for prop, basename, tokenmap, marcsource in chain(
+    for link, basename, tokenmap, marcsource in chain(
             parse_fixed('bib', '000'),
             parse_fixed_by_section('bib', '006'),
             parse_fixed_by_section('bib', '007'),
@@ -46,19 +55,33 @@ def digest_fixedfields(marcframe):
             parse_fixed('hold', '000'),
             parse_fixed('hold', '008'),
             ):
-        colprops.setdefault((prop, basename, tokenmap), []).append(marcsource)
+        colprops.setdefault((link, basename, tokenmap), []).append(marcsource)
 
     prev = None
     for colprop in sorted(colprops):
-        prop, basename, tokenmap = colprop
-        if prev and prop != prev and not (':' in prev and ':' in prop):
+        link, basename, tokenmap = colprop
+        if prev and link != prev and not (':' in prev and ':' in link):
             print()
-        prev = prop
-        if ':' not in prop:
-            prop = ':' + prop
+        prev = link
+        if ':' not in link:
+            link = ':' + link
         basename = ':' + basename if basename else 'UNDEF'
         marcsources = ', '.join(colprops[colprop])
-        print("        ({prop}\t{basename}\tmarc:{tokenmap}) # {marcsources}".format(**vars()))
+        print("        ({link}\t{basename}\tmarc:{tokenmap}) # {marcsources}".format(**vars()))
+
+
+def digest_fields(marcframe, part):
+    defs = marcframe[part]
+    for key, dfn in defs.items():
+        if not key.isdigit():
+            continue # TODO: parse patterns!
+    #dfn.get('match')
+    link = dfn.get('link') or dfn.get('addLink')
+    prop = dfn.get('property') or dfn.get('addProperty')
+    rtype = dfn.get('resourceType')
+    domain = 'UNDEF'
+    marcsources = ', '.join([]) # TODO
+    print("        ({domain}\t{link}\t{rtype}) # {marcsources}".format(**vars()))
 
 
 if __name__ == '__main__':
