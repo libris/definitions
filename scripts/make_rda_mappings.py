e73eb8947a4e46ddd583832143930c6b2ef0c74c
Niklas Lindström <lindstream@gmail.com>
2017-11-10 14:03:16 +0100

    Include some literals in make_rda_mappings output

--- scripts/make_rda_mappings.py
+++ scripts/make_rda_mappings.py
@@ -11,6 +11,8 @@ RDAMediaType = URIRef('http://rdaregistry.info/termList/RDAMediaType')
 RDACarrierType = URIRef('http://rdaregistry.info/termList/RDACarrierType')
 RDAIssuanceType = URIRef('http://rdaregistry.info/termList/ModeIssue')
 
+LANGS = ('en', 'sv')
+
 
 g2 = Graph()
 g2.namespace_manager.bind('owl', OWL)
@@ -21,6 +23,7 @@ g2.namespace_manager.bind('rdacarrier', RDACarrierType)
 g2.namespace_manager.bind('rdaissuance', RDAIssuanceType)
 g2.namespace_manager.bind('kbrda', ID)
 
+
 def make_mappings(rda_type_scheme, rtype):
     data_url = str(rda_type_scheme) + '.ttl'
     g = Graph().parse(data_url, format='turtle')
@@ -30,17 +33,18 @@ def make_mappings(rda_type_scheme, rtype):
             uri = ID[symbol]
             g2.add((uri, RDF.type, OWL.Class))
             g2.add((uri, RDF.type, rtype))
-            g2.add((uri, RDFS.subClassOf, rtype)) # TODO: either type or subClassOf...
+            # TODO: either type or subClassOf...
+            # g2.add((uri, RDFS.subClassOf, rtype))
             g2.add((uri, OWL.sameAs, s))
+            for p2 in (SKOS.definition, SKOS.prefLabel, SKOS.scopeNote):
+                for l in g.objects(s, p2):
+                    if l.language in LANGS:
+                        g2.add((uri, p2, l))
 
     return g
 
 
 content_g = make_mappings(RDAContentType, BF2.Content)
-
-#for content_id in g2.subjects(RDF.type, BF2.Content):
-#    symbol = unicode(content_id).rsplit('/', 1)[-1]
-#    print symbol, BF2[symbol]
 
 media_g = make_mappings(RDAMediaType, BF2.Media)
 
@@ -51,7 +55,7 @@ for top in carrier_g.objects(RDACarrierType, SKOS.hasTopConcept):
         break
 
     assert unicode(top_id).endswith('Carriers(Deprecated)'), top_id
-    #g2.remove((top_id, None, None))
+
     media_id = URIRef(unicode(top_id).replace('Carriers(Deprecated)', ''))
     if unicode(media_id).endswith('ProjectedImage') and (media_id, None, None) not in g2:
         media_id = URIRef(unicode(media_id).replace('ProjectedImage', 'Projected'))
@@ -62,7 +66,7 @@ for top in carrier_g.objects(RDACarrierType, SKOS.hasTopConcept):
     # of the media terms. These broader links are now missing. The code below
     # is thus doesn't execute anymore (no broader link is present in the RDA
     # source data.)
-    # (e.g. kbrda:ComputerChipCartridge a bf2:Carrier; rdfs:subClassOf kbrda:Computer .)
+    # (e.g. kbrda:ComputerChipCartridge rdfs:subClassOf kbrda:Computer .)
     if (media_id, None, None) in g2:
         for narrower in carrier_g.subjects(SKOS.broader, top):
             for specific_carrier in g2.subjects(OWL.sameAs, narrower):
