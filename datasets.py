e6f320cd40fca976fab42a4c4c23bfe355328553
Niklas Lindström <lindstream@gmail.com>
2022-11-29 11:00:17 +0100

    Actually concatenate definitions in legacy mode

--- datasets.py
+++ datasets.py
@@ -1,3 +1,5 @@
+import shutil
+
 import syscore
 import common
 import docs
@@ -6,3 +8,9 @@ if __name__ == '__main__':
     syscore.compiler.main()
     common.compiler.main()
     docs.compiler.main()
+
+    out_dir = syscore.compiler.outdir
+    with (out_dir / 'definitions.jsonld.lines').open('wb') as defs_f:
+        for mod in [syscore, common, docs]:
+            with (out_dir / mod.compiler.union).open('rb') as f:
+                shutil.copyfileobj(f, defs_f)
