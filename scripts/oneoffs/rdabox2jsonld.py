aff943bccfc31ac44c728c34df1c451f160c7446
Niklas Lindström <lindstream@gmail.com>
2025-03-28 12:16:25 +0100

    Extract altLabel, scopeNote, related for RDA terms

--- scripts/oneoffs/rdabox2jsonld.py
+++ scripts/oneoffs/rdabox2jsonld.py
@@ -30,27 +30,18 @@ def convert(doc, rtype):
                 else:
                     continue
 
-                ref = None
-                if content := find_with_content(cells[0]['content'])[-1].get('content'):
-                    for mark in content[0]['marks']:
-                        ref = mark['attrs'].get('href')
-                        if ref:
-                            break
+                (
+                    definition_en, scopenote_en, related_en, altlabel_en
+                ) = extract_properties(cells[1])
 
-                definition_en = None
-                if content := cells[1]['content'][0].get('content'):
-                    definition_en = content[0]['text']
 
                 label_sv = None
                 if content := cells[2]['content'][0].get('content'):
                     label_sv = content[0].get('text')
 
-                definition_sv = None
-
-                for p in cells[3]['content']:
-                    if content := p.get('content'):
-                        definition_sv = ''.join(x['text'] for x in content)
-                    break
+                (
+                    definition_sv, scopenote_sv, related_sv, altlabel_sv
+                ) = extract_properties(cells[3])
 
                 matches = []
                 linkcells = set()
@@ -74,15 +65,36 @@ def convert(doc, rtype):
                                 for x in content
                             )))
 
+                ref = None
+                if content := find_with_content(cells[0]['content'])[-1].get('content'):
+                    for mark in content[0]['marks']:
+                        ref = mark['attrs'].get('href')
+                        if ref:
+                            break
+
                 id_label = clean(label_en) or clean(label_sv)
-                r_id = quote(id_label.title().replace(' ', '').replace('-', ''))
+
+                r_id = label_to_slug(id_label)
+
+                # NOTE: related_sv is probably superfluous
+                # (those labels *should* already be on the targets)
+                related = [
+                    {"@id": label_to_slug(label)}
+                    for label in related_en
+                ]
+
                 item = {
                     "@id": r_id,
                     "@type": rtype,
                     "prefLabel_en": clean(label_en),
                     "prefLabel_sv": clean(label_sv),
+                    "altLabel_en": clean(altlabel_en),
+                    "altLabel_sv": clean(altlabel_sv),
                     "definition_en": clean(definition_en),
                     "definition_sv": clean(definition_sv),
+                    "scopeNote_en": clean(scopenote_en),
+                    "scopeNote_sv": clean(scopenote_sv),
+                    "related": related,
                     "exactMatch": {"@id": ref} if ref else None,
                     "closeMatch": matches,
                     "note": notes
@@ -92,6 +104,51 @@ def convert(doc, rtype):
     return {item['@id']: item for item in items}
 
 
+def extract_properties(cell):
+    definition = None
+    # "Scope Note:"
+    scopenote = None
+    # "Related Terms:" (bullet list)
+    related = []
+    # "Synonyms:"
+    altlabel = None
+
+    next_is = None
+
+    for p in cell['content']:
+        if content := p.get('content'):
+            text = ''.join(t for x in content if (t := x.get('text')))
+            if definition is None:
+                definition = text
+            elif next_is == 'synonyms':
+                altlabel = text
+                next_is = None
+            elif next_is == 'scope note':
+                scopenote = text
+                next_is = None
+            elif next_is == 'related terms':
+                if text:
+                    related.append(text)
+                else:
+                    assert p['type'] == 'bullet_list'
+                    items = [
+                        y['text']
+                        for li in content
+                        for x in li['content']
+                        for y in x['content']
+                    ]
+                    related += items
+                next_is = None
+            else:
+                for next_is in ['scope note', 'synonyms', 'related terms']:
+                    if next_is in text.lower():
+                        break
+                else:
+                    next_is = None
+
+    return definition, scopenote, related, altlabel
+
+
 def find_with_content(items):
     return [item for item in items if 'content' in item]
 
@@ -99,6 +156,10 @@ def find_with_content(items):
 def to_type(fname):
     fname = fname.rsplit('/')[-1].rsplit('.')[0]
     return re.sub(r'\W', '', fname.title().replace(' ', ''))
+
+
+def label_to_slug(label):
+    return quote(label.title().replace(' ', '').replace('-', ''))
 
 
 def clean(s):
@@ -151,7 +212,11 @@ def main():
             "prefLabel_sv": {"@id": "prefLabel", "@language": "sv"},
             "prefLabel_en": {"@id": "prefLabel", "@language": "en"},
             "definition_sv": {"@id": "definition", "@language": "sv"},
-            "definition_en": {"@id": "definition", "@language": "en"}
+            "definition_en": {"@id": "definition", "@language": "en"},
+            "altLabel_sv": {"@id": "altLabel", "@language": "sv"},
+            "altLabel_en": {"@id": "altLabel", "@language": "en"},
+            "scopeNote_sv": {"@id": "scopeNote", "@language": "sv"},
+            "scopeNote_en": {"@id": "scopeNote", "@language": "en"},
         },
         "@graph": list(result_map.values())
     }
