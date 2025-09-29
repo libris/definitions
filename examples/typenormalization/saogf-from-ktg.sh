7884e882f283059575f1be6317da149f93a5befd
Niklas Lindström <lindstream@gmail.com>
2025-09-29 19:36:50 +0200

    Move saogf-related files to source/saogf

--- examples/typenormalization/saogf-from-ktg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd $(dirname $0)/../../
-(
-  oxrq -f examples/typenormalization/saogf-insert-missing.ru source/categories/{contentforms,contentgenres,genreforms,marcmatches,marcmatches-music}.ttl
-  cat examples/typenormalization/saogf-patches.ttl
-) | oxrq -itrig -f examples/typenormalization/saogf-from-ktg.rq | trld -ittl -rottl
